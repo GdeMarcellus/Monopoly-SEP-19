80148510ea5f8586a1d0b3cbf7a32601f26cdad3
GPGD <98532892+GPGD@users.noreply.github.com>
2022-02-23 16:01:53 +0000

    Implemented Dice class

--- src/backend/Dice.java
+++ src/backend/Dice.java
@@ -1,10 +1,49 @@
 package backend;
 
-public class Dice {
-    /*
-    int array [numDice]
+import java.util.concurrent.ThreadLocalRandom;
 
-    Method: roll dice
-    Method: get dice value
+public class Dice {
+    int numDice; //Number of dice
+    int diceMin; //smallest num on dice
+    int diceMax; //largest num on dice
+
+    int dice[]; //array of dice
+
+    public Dice(int numDice, int diceMin, int diceMax){
+        this.numDice = numDice;
+        this.diceMin = diceMin;
+        this.diceMax = diceMax;
+    }
+
+    /**
+     * Sets each 'dice' in the array to a randInt between diceMin & diceMax (inclusive)
+     * @param void
+     * @return void
      */
+    public void rollDice() {
+        for (int i = 0; i < numDice; i++) {
+            dice[i] = ThreadLocalRandom.current().nextInt(diceMin, diceMax + 1);
+        }
+    }
+
+    /***
+     * returns array of dice (intergers)
+     * @param void
+     * @return array of dice (intergers)
+     */
+    public int[] getDiceValues() {
+        return dice;
+    }
+
+    public void setNumDice(int numDice) {
+        this.numDice = numDice;
+    }
+
+    public void setDiceMax(int diceMax) {
+        this.diceMax = diceMax;
+    }
+
+    public void setDiceMin(int diceMin) {
+        this.diceMin = diceMin;
+    }
 }
