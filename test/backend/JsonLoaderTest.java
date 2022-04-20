d02cc4ab6cfe74e0b782939faad510bdd2ba0711
Gregoire de Marcellus <gdemarcellus@gmail.com>
2022-04-20 16:11:56 +0200

    Create JsonLoaderTest.java

---
+++ test/backend/JsonLoaderTest.java
@@ -0,0 +1,41 @@
+package backend;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JsonLoaderTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testStartUp() {
+    }
+
+    @Test
+    public void testSetUpCard() {
+    }
+
+    @Test
+    public void testItterateEffects() {
+    }
+
+    @Test
+    public void testSetUpTiles() {
+    }
+
+    @Test
+    public void testBuildBuildingTile() {
+    }
+
+    @Test
+    public void testBuildStationTile() {
+    }
+
+    @Test
+    public void testBuildUtilityTile() {
+
+    }
+}
