fa6d2b89e2c1f3d2e712f5d0cf31c2ba11a6cfd0
DoveCrying <zenolino@gmail.com>
2022-04-29 01:16:17 +0200

    Made launchable

---
+++ src/GUI/Launcher.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javafx.application.Application;
+
+public class Launcher {
+    static public void main(String[] args)
+    {
+        Application.launch(GUI.class,args);
+    }
+}
